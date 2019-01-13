075ed19e727b367e7085e85c6120cf28df13f28c
ifmiss <185098535@qq.com>
2019-01-13 23:35:13 +0800

    删除不必要的代码

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
+const webpack = require('webpack')
 const dev = require('./webpack.dev')
 const prod = require('./webpack.prod')
-// const common = require('./webpack.common')
-// console.log(dev)
-console.log(process.env.NODE_ENV)
 switch (process.env.NODE_ENV) {
 	case 'development':
 		module.exports = dev
