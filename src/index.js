1cfe5ed97b995c0b7612c2c1c0a9ee56469b0aad
ifmiss <185098535@qq.com>
2019-01-27 23:56:54 +0800

    移除wx-audio的测试引入less

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import './style.scss'
 import DJS from 'd-js-utils'
-import WxAudio from 'wx-audio'
-console.log(WxAudio)
-const a = new WxAudio ({
-  ele: '.audio'
-})
 
 DJS.utils.console(`Webpack通用环境`)
